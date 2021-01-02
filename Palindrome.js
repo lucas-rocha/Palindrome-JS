21b2c8e6af2211729498a2d0eb56d7db4dcf193d
Lucas Rocha <lucasrocha.dv@gmail.com>
2021-01-02 18:21:08 -0300

    Initial commit

---
+++ Palindrome.js
@@ -0,0 +1,17 @@
+function isPalindrome(string) {
+  let re = /[\W_]/g
+  let str = string.toLowerCase().replace(re, '')
+  let len = str.length
+  console.log(str)
+  for(let i = 0; i < len / 2; i++) {
+    console.log(i)
+    if(str[i] !== str[len - 1 - i]) {
+      return false
+    }
+  }
+  return true
+}
+
+console.log(isPalindrome('Luz azul')) // Deve retornar true
+console.log(isPalindrome('O lobo ama o bolo'))  // Deve retornar true
+console.log(isPalindrome('Oi')) // Deve retornar false
